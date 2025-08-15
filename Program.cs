2e77a2668bf2b3c55f5af42a1436b7a03334cc1a
KillSwitch <ggouvouniotis@proton.me>
2025-08-15 12:13:51 -0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var fizzBuzz = new FizzBuzzChallenge();
+
+        Console.WriteLine("Enter a number:");
+        if (int.TryParse(Console.ReadLine(), out int number))
+        {
+            Console.WriteLine(fizzBuzz.FizzBuzz(number));
+        }
+        else
+        {
+            Console.WriteLine("Invalid input.");
+        }
+    }
+}
+
+public class FizzBuzzChallenge
+{
+    public string FizzBuzz(int number)
+    {
+        if (number % 3 == 0 && number % 5 == 0)
+            return "fizzbuzz";
+
+        if (number % 3 == 0)
+            return "fizz";
+
+        if (number % 5 == 0)
+            return "buzz";
+
+        return number.ToString();
+    }
+}
